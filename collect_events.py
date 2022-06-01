09580223a51ce1a8f8bfffc3cfb09bc68fa57962
Frank Casanova <frankcasanova.info@gmail.com>
2022-06-01 21:39:16 +0200

    app logic done

---
+++ collect_events.py
@@ -0,0 +1,40 @@
+from typing import Iterator, Dict
+import datetime as _dt
+import scraper as _scraper
+import json as _json
+
+
+def _date_range(start_date: _dt.date, end_date: _dt.date) -> Iterator[_dt.date]:
+    """
+    A generator that yields dates between start_date and end_date.
+    ::param start_date: The first date to yield.
+    ::param end_date: The last date to yield.
+    """
+    current_date = start_date
+    while current_date <= end_date:
+        yield current_date
+        current_date += _dt.timedelta(days=1)
+
+
+def create_events_dict() -> Dict:
+    """
+    A function that creates a dictionary of events.
+    """
+    events = dict()
+    start_date = _dt.date(2021, 1, 1)
+    end_date = _dt.date(2021, 12, 31)
+
+    for date in _date_range(start_date, end_date):
+        month = date.strftime("%B").lower()
+        if month not in events:
+            events[month] = dict()
+
+        events[month][date.day] = _scraper.events_of_the_day(month, date.day)
+
+    return events
+
+
+if __name__ == "__main__":
+    events = create_events_dict()
+    with open('events.json', 'w', encoding='utf-8') as f:
+        _json.dump(events, f, ensure_ascii=False, indent=4)
---
+++ scraper.py
@@ -0,0 +1,40 @@
+import requests as _requests
+import bs4 as _bs4
+
+
+def _generate_url(month: str, day: int) -> str:
+    """
+    Generates a url for the given month and day.
+    ::param month: The month to generate the url for.
+    ::param day: The day to generate the url for.
+
+    ::return: The generated url.
+    """
+    return f"https://www.onthisday.com/day/{month}/{day}"
+
+
+def _get_page(url: str) -> str:
+    """
+    Gets the page at the given url.
+    ::param url: The url to get the page from.
+
+    ::return: The page at the given url.
+    """
+    page = _requests.get(url)
+    soup = _bs4.BeautifulSoup(page.content, "html.parser")
+    return soup
+
+
+def events_of_the_day(month: str, day: int) -> list[str]:
+    """
+    Gets the events of the day at the given month and day.
+    ::param month: The month to get the events for.
+    ::param day: The day to get the events for.
+
+    ::return: The events of the day at the given month and day.
+    """
+    url = _generate_url(month, day)
+    soup = _get_page(url)
+    raw_events = soup.find_all(class_="event")
+    events = [event.text for event in raw_events]
+    return events
