1b2ecadab2340834b1a437246520db901522f957
frankcasanova <frankcasanova.info@gmail.com>
2022-06-07 13:57:24 +0200

    deploy version

--- app/views.py
+++ app/views.py
@@ -6,38 +6,43 @@ from .services import get_today
 
 async def all_events():
     """
-    return all events
-
-    ::return: dict
+    # [Show all events]
+    This path operation shows all events in the app
+    
+    ### Returns a json with the all events in the app.
     """
     return get_all_events()
 
 
 async def today_events():
-    """_summary_
-    return events for today
-
-    ::return: dict
     """
+    # [Show all events for today]
+    This path operation shows all events for today
+   
+    ### Returns a json with the all events for today.
+    """
+   
     return get_today()
 
 
 async def month_events(month: str):
     """
-    return events for a specific month
-
-    : : param month: str
-    : : return: dict
+    # [Show all events for a month]
+    this path operation shows all events for a specific month
+    ### Parameters:
+    - month: str
+    ### Returns a json with the all events for a specific month.
     """
     return get_month_events(month.lower())
 
 
 async def day_events(month: str, day: int):
     """
-    return events for a specific day
-
-    : : param month: str
-    : : param day: int
-    : : return: dict
+    # [Show all events for a especific day]
+    this path operation shows all events for a specific day
+    ### Parameters:
+    - month: str
+    - day: int
+    ### Returns a json with the all events for a specific day.
     """
     return get_day_events(month.lower(), day)
