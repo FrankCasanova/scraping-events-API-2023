9bcf771bc8ca57f62d88bb92485bc798096eb12f
Frank Casanova <frankcasanova.info@gmail.com>
2023-10-16 19:34:23 +0200

    origins of the middelware set to all

--- main.py
+++ main.py
@@ -3,25 +3,20 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.endpoints import router
 
-ORIGINS = ["http://localhost", "http://localhost:8080", "herokuapp.com"]
-
-
 def include_router(app: FastAPI) -> FastAPI:
     app.include_router(router)
     return app
-
 
 def start_app() -> FastAPI:
     app = FastAPI()
     app = include_router(app)
     return app
 
-
 app = start_app()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ORIGINS,
+    allow_origins=["*"],	
     allow_credentials=True,
     allow_methods=["GET"],
     allow_headers=["*"],
